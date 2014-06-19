bd5fc2f83c48801a707ecccd95adf1640c65bf02
Thanh Vu <ttvu90@gmail.com>
2014-06-19 11:36:02 +0700

    Structure
    create a structure

---
+++ YYScrollingTableViewCell/YYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YYAppDelegate.h
+//  YYScrollingTableViewCell
+//
+//  Created by yoyo on 6/19/14.
+//  Copyright (c) 2014 yoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YYScrollingTableViewCell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YYScrollingTableViewCell
+//
+//  Created by yoyo on 6/19/14.
+//  Copyright (c) 2014 yoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YYAppDelegate class]));
+    }
+}
